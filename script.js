4c1ba113a58bbb02dfa2070be733b07646fe3398
Ahmed Alsaedi <aalsaedi265@gmail.com>
2022-01-16 14:13:59 -0600

    The first update to the website, preWork content

---
+++ script.js
@@ -0,0 +1,13 @@
+// const h2= document.createElement("h3");
+// h2.textContent="Alternative spot to find my work or reach me";
+// document.querySelector("body").appendChild(h2);
+
+const hub= document.getElementById("MyGitHub");
+hub.addEventListener('click', function(){
+    alert('You will be departing this page')
+});
+
+const lin= document.getElementById("link");
+lin.addEventListener('click', function(){
+    alert('You will be departing this page')
+});
